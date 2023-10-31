78180debd37e2df1060e1682a005b5ce0e97a4f1
Nora <adrianaguirrepetro@gmail.com>
2023-10-30 20:15:10 -0500

    endpoints: get, put, delete

--- src/main/java/com/users/UsersApplication.java
+++ src/main/java/com/users/UsersApplication.java
@@ -3,10 +3,10 @@ package com.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -18,11 +18,22 @@ public class UsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-	@GetMapping ("/api/v1/users")
+	@GetMapping ("/api/v1/users") //Debuelve la lista de usuarios almacenados
 	public List<Users> getUsers () {
 		return usersRepository.findAll();
-
 	}
 
+	@GetMapping("/api/v1/users/{id}")
+	public Optional<Users> findById(@PathVariable Integer id) {
+		return usersRepository.findById(id);
+	}
+	@PutMapping
+	public Users update(@RequestBody Users users) {
+		return usersRepository.save(users);
+	}
+	@DeleteMapping("/api/v1/users/{id}")
+	public void deleteById(@PathVariable Integer id) {
+		usersRepository.deleteById(id);
+	}
 
 }
