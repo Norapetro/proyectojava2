6d196194b636e0f1b7a20ff242c8b5ff43ca7d24
Nora <adrianaguirrepetro@gmail.com>
2023-11-02 21:05:32 -0500

    probando funcionalidad

--- src/main/java/com/users/UsersApplication.java
+++ src/main/java/com/users/UsersApplication.java
@@ -3,6 +3,7 @@ package com.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,22 +19,27 @@ public class UsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-	@GetMapping ("/api/v1/users") //Debuelve la lista de usuarios almacenados
+	@GetMapping ("/api/v1/users") //Debuelve la lista de usuarios almacenados ok
 	public List<Users> getUsers () {
 		return usersRepository.findAll();
 	}
 
-	@GetMapping("/api/v1/users/{id}")
+	@GetMapping("/api/v1/users/{id}") //debuelve por el ID ok
 	public Optional<Users> findById(@PathVariable Integer id) {
 		return usersRepository.findById(id);
 	}
+
+	//PUT
 	@PutMapping
 	public Users update(@RequestBody Users users) {
 		return usersRepository.save(users);
 	}
+
+
+	//DELETE
 	@DeleteMapping("/api/v1/users/{id}")
 	public void deleteById(@PathVariable Integer id) {
-		usersRepository.deleteById(id);
+		usersRepository.deleteById(id); //No retorna nada porque estamos eliminando
 	}
 
 }
